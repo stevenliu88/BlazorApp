1c699e3f1f27c49aa738a57cf1bade00f4d2c9d0
stevenliu88 <30949641+stevenliu88@users.noreply.github.com>
2021-11-03 16:10:55 +1100

    Add employeeList and display on the webpage

--- BlazorTutorial/EmployeeManagement.Web/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/EmployeeList.razor.g.cs
+++ BlazorTutorial/EmployeeManagement.Web/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/EmployeeList.razor.g.cs
@@ -13,70 +13,70 @@ namespace EmployeeManagement.Web.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\liu1\Desktop\blazorLearning\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
+#line 1 "C:\Users\liu1\Desktop\blazorLearning\BlazorApp\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\liu1\Desktop\blazorLearning\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
+#line 2 "C:\Users\liu1\Desktop\blazorLearning\BlazorApp\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\liu1\Desktop\blazorLearning\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
+#line 3 "C:\Users\liu1\Desktop\blazorLearning\BlazorApp\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\liu1\Desktop\blazorLearning\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
+#line 4 "C:\Users\liu1\Desktop\blazorLearning\BlazorApp\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\liu1\Desktop\blazorLearning\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
+#line 5 "C:\Users\liu1\Desktop\blazorLearning\BlazorApp\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\liu1\Desktop\blazorLearning\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
+#line 6 "C:\Users\liu1\Desktop\blazorLearning\BlazorApp\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\liu1\Desktop\blazorLearning\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
+#line 7 "C:\Users\liu1\Desktop\blazorLearning\BlazorApp\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\liu1\Desktop\blazorLearning\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
+#line 8 "C:\Users\liu1\Desktop\blazorLearning\BlazorApp\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
 using EmployeeManagement.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\liu1\Desktop\blazorLearning\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
+#line 9 "C:\Users\liu1\Desktop\blazorLearning\BlazorApp\BlazorTutorial\EmployeeManagement.Web\_Imports.razor"
 using EmployeeManagement.Web.Shared;
 
 #line default
 #line hidden
 #nullable disable
     [Microsoft.AspNetCore.Components.RouteAttribute("/")]
-    public partial class EmployeeList : Microsoft.AspNetCore.Components.ComponentBase
+    public partial class EmployeeList : EmployeeListBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
